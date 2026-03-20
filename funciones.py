0a7ef9b8bfbc2be5885ad10d59eb272364cffcbb
luis suarez <luismasuarezsevilla@gmail.com>
2026-03-20 18:01:38 -0500

    se agregan funciones

--- funciones.py
+++ funciones.py
@@ -66,3 +66,49 @@ def register_products():
     product = (Product_id, Product_name, Unit_price)
 
     products = products + (product,)
+
+    valid = False
+
+    while valid == False:
+        try:
+            quantity_product = int(input("Ingrese el valor del precio unitario: "))
+
+            if quantity_product > 0:
+                valid = True
+
+            else:
+                print("La cantidad debe ser mayor a cero")
+
+        except:
+            print("Error ingrese numeros sin puntos ni comas")
+
+
+def total_delivery (price_unit, quantity ):
+    return round(price_unit * quantity, 1) 
+
+def creation_delivery():
+    client = register_clients ()
+    products = register_products ()
+    total = total_delivery()
+
+    delivery= {
+        "client": client,
+        "products": products,
+        "total": total
+    }
+    
+    print("pedido creado correctamente")
+    print(delivery)
+    
+
+def consult_of_delivery ():
+    print("consulta de productos: \n")
+
+    if not delivery:
+        print("no hay pedidos registrados \n")
+        input("presione cualquier tecla para volver al menu: \n")
+        return
+    
+    for i, d in enumerate(delivery, 1):
+        print(f"{i}. {d('clients')} | {d('products')} | {d('total')}")
+    input("presione cualquier tecla para volver al menu")
